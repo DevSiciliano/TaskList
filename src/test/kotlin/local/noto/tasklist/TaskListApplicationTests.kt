228ff2537d5a01804bcf24c8f709bf96595511f9
DevSiciliano <m.noto@ikmail.com>
2026-08-19 11:30:22 +0200

    chore: initial commit

---
+++ src/test/kotlin/local/noto/tasklist/TaskListApplicationTests.kt
@@ -0,0 +1,13 @@
+package local.noto.tasklist
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaskListApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
